13209af5fcc30a3b2e6eb8b73d45fb1b9273bd50
mruifer <mruifer885@gmail.com>
2025-04-17 20:41:58 +0200

    Add files via upload

---
+++ imagenes.m
@@ -0,0 +1,216 @@
+gen_part;
+yp1=yp;
+h=L/(N+1);
+
+for j=1:M
+    % Velocidades
+    [vp]=evop(Wy(:,j),Wry(:,j),Wiy(:,j),h,yi,yp1);
+    for i=1:length(yp1)
+        if yp1(i)>(yi+L-3)
+            vp1(i)=0;
+            yp1(i)=yi+L-1;
+        end
+    end
+    % Posiciones
+    yp1=yp1+vp*k;
+    % Representación de la función de onda y las particulas evolucionado
+    plot(y,Wty(:,j))
+    hold on
+    scatter(yp1,valvertical)
+    xlim([-10 (yi+L-5)]);
+    ylim([0 0.35]);
+    hold off
+    A=getframe();
+    if j==1
+        figure(2)
+        scatter(yp1,valvertical)
+        xlabel('Position (Å)')
+        xlim([-10 10]);
+        yticks([]);
+        set(gca, 'YColor', 'none');
+        saveas(gcf, 'Imagen11.jpg')
+        figure(3)
+        plot(y,Wty(:,j))
+        hold on
+        plot(y,Wry(:,j))
+        plot(y,Wiy(:,j))
+        xlabel('Position (Å)')
+        xlim([-10 10]);
+        ylim([-0.4 0.4]);
+        legend('Probability density','Real-part wave-function','Imaginary-part wave-function','Location','best')
+        yline(0,'k','LineWidth',1,'HandleVisibility','off')
+        saveas(gcf, 'Imagen7.jpg')
+        hold off
+    end
+    if j==300
+        figure(2)
+        scatter(yp1,valvertical)
+        xlabel('Position (Å)')
+        xlim([5 25]);
+        yticks([]);
+        set(gca, 'YColor', 'none');
+        saveas(gcf, 'Imagen12.jpg')
+        figure(3)
+        plot(y,Wty(:,j))
+        hold on
+        plot(y,Wry(:,j))
+        plot(y,Wiy(:,j))
+        xlabel('Position (Å)')
+        xlim([5 25]);
+        ylim([-0.4 0.4]);
+        legend('Probability density','Real-part wave-function','Imaginary-part wave-function','Location','best')
+        yline(0,'k','LineWidth',1,'HandleVisibility','off')
+        saveas(gcf, 'Imagen8.jpg')
+        hold off
+    end
+    if j==301
+        figure(2)
+        scatter(yp1,valvertical)
+        xlabel('Position (Å)')
+        xlim([5 25]);
+        yticks([]);
+        set(gca, 'YColor', 'none');
+        saveas(gcf, 'Imagen13.jpg')
+        figure(3)
+        plot(y,Wty(:,j))
+        hold on
+        plot(y,Wry(:,j))
+        plot(y,Wiy(:,j))
+        xlabel('Position (Å)')
+        xlim([5 25]);
+        ylim([-0.4 0.4]);
+        legend('Probability density','Real-part wave-function','Imaginary-part wave-function','Location','best')
+        yline(0,'k','LineWidth',1,'HandleVisibility','off')
+        saveas(gcf, 'Imagen9.jpg')
+        hold off
+    end
+    if j==600
+        figure(2)
+        scatter(yp1,valvertical)
+        xlabel('Position (Å)')
+        xlim([-10 10]);
+        yticks([]);
+        set(gca, 'YColor', 'none');
+        saveas(gcf, 'Imagen14.jpg')
+        figure(3)
+        plot(y,Wty(:,j))
+        hold on
+        plot(y,Wry(:,j))
+        plot(y,Wiy(:,j))
+        xlabel('Position (Å)')
+        xlim([-10 10]);
+        ylim([-0.4 0.4]);
+        legend('Probability density','Real-part wave-function','Imaginary-part wave-function','Location','best')
+        yline(0,'k','LineWidth',1,'HandleVisibility','off')
+        saveas(gcf, 'Imagen10.jpg')
+        hold off
+        break
+    end
+end
+
+terminar=false;
+figure(1)
+for j=1:M
+    % Velocidades
+    [vp]=evop(Uy(:,j),Ury(:,j),Uiy(:,j),h,yi,yp);
+    % Si los puntos no se salen del intervalo de solución termina
+    for i=1:length(yp)
+        if yp(i)>(yi+L-3)
+            terminar=true;
+            break
+        end
+    end
+    if terminar==true
+        break
+    end
+    % Posiciones
+    yp=yp+vp*k;
+    % Representación de la función de onda y las particulas evolucionado
+    plot(y,Uty(:,j))
+    hold on
+    scatter(yp,valvertical)
+    xlim([-10 (yi+L-5)]);
+    ylim([0 0.35]);
+    hold off
+    A=getframe();
+    if j==1
+        figure(2)
+        scatter(yp,valvertical)
+        xlabel('Position (Å)')
+        xlim([-5 5]);
+        yticks([]);
+        set(gca, 'YColor', 'none');
+        saveas(gcf, 'Imagen4.jpg')
+        figure(3)
+        plot(y,Uty(:,j))
+        hold on
+        plot(y,Ury(:,j))
+        plot(y,Uiy(:,j))
+        xlabel('Position (Å)')
+        xlim([-5 5]);
+        ylim([-0.4 0.4]);
+        legend('Probability density','Real-part wave-function','Imaginary-part wave-function','Location','best')
+        yline(0,'k','LineWidth',1,'HandleVisibility','off')
+        saveas(gcf, 'Imagen1.jpg')
+        hold off
+    end
+    if j==75
+        figure(2)
+        scatter(yp,valvertical)
+        xlabel('Position (Å)')
+        xlim([-1.25 8.75]);
+        yticks([]);
+        set(gca, 'YColor', 'none');
+        saveas(gcf, 'Imagen5.jpg')
+                figure(3)
+        plot(y,Uty(:,j))
+        hold on
+        plot(y,Ury(:,j))
+        plot(y,Uiy(:,j))
+        xlabel('Position (Å)')
+        xlim([-1.25 8.75]);
+        ylim([-0.4 0.4]);
+        legend('Probability density','Real-part wave-function','Imaginary-part wave-function','Location','best')
+        yline(0,'k','LineWidth',1,'HandleVisibility','off')
+        saveas(gcf, 'Imagen2.jpg')
+        hold off
+    end
+    if j==150
+        figure(2)
+        scatter(yp,valvertical)
+        xlabel('Position (Å)')
+        xlim([2.5 12.5]);
+        yticks([]);
+        set(gca, 'YColor', 'none');
+        saveas(gcf, 'Imagen6.jpg')
+        figure(3)
+        plot(y,Uty(:,j))
+        hold on
+        plot(y,Ury(:,j))
+        plot(y,Uiy(:,j))
+        xlabel('Position (Å)')
+        xlim([2.5 12.5]);
+        ylim([-0.4 0.4]);
+        legend('Probability density','Real-part wave-function','Imaginary-part wave-function','Location','best')
+        yline(0,'k','LineWidth',1,'HandleVisibility','off')
+        saveas(gcf, 'Imagen3.jpg')
+        hold off
+        break
+    end
+end
+
+
+
+%{
+plot(y,Uty(:,75))
+hold on
+plot(y,Ury(:,75))
+plot(y,Uiy(:,75))
+xlabel('Position')
+xlim([-1.25 8.75]);
+ylim([-0.4 0.4]);
+legend('Probability density','Real-part wave-function','Imaginary-part wave-function','Location','best')
+yline(0,'k','LineWidth',1,'HandleVisibility','off')
+saveas(gcf, 'Imagen2.jpg')
+hold off
+%}
---
+++ llamadaev.m
@@ -0,0 +1,71 @@
+%% LLamada evolución
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Parámetros
+% * k_ = velocidad inicial en el eje _
+% * L = longitud del intervalo.
+% * xi = primer punto del eje
+% * uini = condición inicial u(x,0).
+% * alfa = constante de la ecuación.
+% * froni = u(0,t).
+% * frond = u(xfinal,t).
+% * N = número de discretación (pasos en x).
+% * M = número de iteraciones en tiempo (pasos en t).
+% * T = intervalo de tiempo.
+%%%%%%%%%%%%%%%%%%%%%%%%
+% Salida
+% * x = vector de puntos en x.
+% * t = vector de puntos en t.
+% * U = ecuación solución de la evolución.
+%%%%%%%%%%%%%%%%%%%%%%%%
+% Información
+% * Velocidad en el eje y: Energía=10 eV -> p0y=raiz(2mE)=5.14
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+hb=1;
+m=1;
+ky=5.14/hb;
+alfa=1i*hb/(2*m);
+% Parámetros espaciales
+yi=-10;
+L=80;
+%N=99999;
+%N=2^18-1;
+N=399999;
+h=L/(N+1);
+y=yi:h:yi+L;
+% Parámetros temporales
+T=6;
+M=599;
+k=T/M;
+t=0:k:T;
+% Vectores donde se almacenarán las soluciones
+Uy=zeros(N+2,M+1);
+% Condiciones de frontera
+froni=@(t)(0);
+frond=@(t)(0);
+Uy(1,1)=froni(0);
+Uy(N+2,1)=frond(0);
+% Funciones en el instante inicial (velocidad inicial en el eje y)
+ uiniy=@(y)1/sqrt(2*pi)*(exp(1i*ky*y).*exp(-y.^2/2));
+% uiniy=@(y)1/sqrt(2*pi)*(exp(1i*ky*y).*exp(-y.^4));
+anch=2;
+% uiniy=@(y)((-anch<y&y<=0).*(y/anch+1)+(y>0&y<anch).*(1-y/anch))/anch.*exp(1i*ky*y);
+% uiniy=@(y)((abs(y)<anch).*(anch^2-y.^2)/(anch^3*4/3)).*exp(1i*ky*y);
+Uy(2:N+1,1)=uiniy(y(2:N+1));
+% Resolvemos la ecuación
+[Uy]=crank_nicolson_part(Uy,h,k,alfa,froni,frond,N,M);
+% Soluciones
+Ury=real(Uy);
+Uiy=imag(Uy);
+Uty=abs(Uy).^2;
+% Inversión temporal
+instante=300;
+Minv=M-instante;
+%Wy=zeros(N+2,Minv+1);
+Wyini=conj(Uy(:,instante));
+% Resolvemos la ecuación
+[Wy]=crank_nicolson_part(Wyini,h,k,alfa,froni,frond,N,Minv);
+Wy=[Uy(:,1:instante),Wy];
+% Soluciones
+Wry=real(Wy);
+Wiy=imag(Wy);
+Wty=abs(Wy).^2;
